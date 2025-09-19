35b889ca6f80423d921c983079e4dc949ae0cd9e
hayeon <k41482962@gmail.com>
2025-09-20 00:57:59 +0900

    [GEN-800] ui 변경 - 시작 화면 추가 - 페이지 연결 경로 변경 - 메인 페이지 드롭다운 연결 - 그룹 모달창(구현중)

---
+++ websubscriber/src/Component/layouts/MainLayout.js
@@ -0,0 +1,16 @@
+// 메인 화면 레이아웃
+
+import {FaAngleDown} from "react-icons/fa";
+import React from "react";
+import './MainLayout.css';
+
+function MainLayout(){
+
+    return(
+        <div className='header-bar'>
+            <h1 className='font-title'>SCMS</h1>
+        </div>
+    );
+}
+
+export default MainLayout;

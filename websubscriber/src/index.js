bb9fdf7dfed53c5135e24966c7155efc9cd0728b
hayeon <jhy414800@cu.ac.kr>
2025-09-27 23:24:41 +0900

    [GEN-800] 프론트 <-> 백 연결

--- websubscriber/src/index.js
+++ websubscriber/src/index.js
@@ -1,11 +1,13 @@
-import App from './App';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+// index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Visualize from "./Panel/Visualize";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 import { ProSidebarProvider } from "react-pro-sidebar";
-import {Provider} from "react-redux";
-import {store} from "./app/store";
+import { store } from "./app/store";
+
+import App from "./App";
+import Start from "./Page/Start";
 import Login from "./Page/Login";
 import SignUp from "./Page/SignUp";
 import Password from "./Page/Password";
@@ -13,80 +15,36 @@ import RePassword from "./Page/RePassword";
 import ID from "./Page/ID";
 import CheckID from "./Page/CheckID";
 import Dashboard from "./Page/Dashboard";
-import Start from "./Page/Start";
 import MainPg from "./Page/MainPg";
 import MainPgGroup from "./Page/MainPgGroup";
-import SettingTab from "./Component/Setting/SettingTab";
 import Setting from "./Page/Setting";
 import SettingGroup from "./Page/SettingGroup";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
-);
-
 const router = createBrowserRouter([
-	{
+    {
         path: "/",
-        element: <Start/>
-    },{
-        path: "sign_up",
-        element: <SignUp/>
-    }, {
-        path: "login",
-        element: <Login/>
-    },{
-        path: "password",
-        element: <Password/>
-    },{
-        path: "reset_password",
-        element: <RePassword/>
-    },{
-        path: "id",
-        element: <ID/>
-    },{
-        path: "check_id",
-        element: <CheckID/>
-    },{
-        path: "dashboard",
-        element: <Dashboard/>
-    },{
-        path: "main",
-        element: <MainPg/>
-    },{
-        path: "group/main",
-        element: <MainPgGroup/>
-    },{
-        path: "/setting",
-        element: <Setting/>
-    },{
-        path: "/group/setting",
-        element: <SettingGroup/>
+        element: <App />,
+        children: [
+            { index: true, element: <Start /> },
+            { path: "sign_up", element: <SignUp /> },
+            { path: "login", element: <Login /> },
+            { path: "password", element: <Password /> },
+            { path: "reset_password", element: <RePassword /> },
+            { path: "id", element: <ID /> },
+            { path: "check_id", element: <CheckID /> },
+            { path: "dashboard", element: <Dashboard /> },
+            { path: "main", element: <MainPg /> },
+            { path: "group/main", element: <MainPgGroup /> },
+            { path: "setting", element: <Setting /> },
+            { path: "group/setting", element: <SettingGroup /> },
+        ],
     },
-
 ]);
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-    // <React.StrictMode> <-- re-render twice error
     <Provider store={store}>
         <RouterProvider router={router} />
     </Provider>
-    // </React.StrictMode>
 );
-
-
-// 이전페이지
-// 	path: "/",
-// 	element: <IpInputPage />
-// },{
-//     path: "main",
-//     element: <MainPage/>
-// },{
-//     path: "visualize",
-//     element: <Visualize/>
-// },{
-//     path: "kakaomap",
-//     element: <Kakaomap/>
-// },{
-//     path: "Combined",
-//     element: <Combined/>
-// },{
---
+++ websubscriber/src/shared/api/client.ts
@@ -0,0 +1,14 @@
+// src/shared/api/client.js
+import axios from '../../Page/TestApi';
+
+export const api = axios.create({
+    baseURL: process.env.REACT_APP_API_URL,
+});
+
+export const setAuthToken = (token) => {
+    if (token) {
+        api.defaults.headers.common.Authorization = `Bearer ${token}`;
+    } else {
+        delete api.defaults.headers.common.Authorization;
+    }
+};
